96623faeffca73c32b732ca7a8a42795541ec94f
Nikki Goel <gupta.nikkigupta@gmail.com>
2019-11-24 21:19:07 +0530

    added fonts

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,25 +13,46 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
-        overflow: TextOverflow.fade,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: GridTile(
+        child: Container(
+          padding: const EdgeInsets.all(7.0),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.4),
+                blurRadius: 20.0,
+                spreadRadius: 5.0,
+                offset: Offset(10, 10),
+              )
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+        footer: GridTileBar(
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.fade,
+          ),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
         ),
       ),
     );
