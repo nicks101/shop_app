3f3917170bed34da59bfb70b3dee3ac39c72da3a
Nikki Goel <gupta.nikkigupta@gmail.com>
2021-01-02 22:08:50 +0530

    small changes in Ui

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -8,22 +8,6 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
           builder: builder,
           settings: settings,
         );
-
-  @override
-  Widget buildTransitions(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) {
-    if (settings.name == '/products-overview-screen') {
-      return child;
-    }
-    return FadeTransition(
-      opacity: animation,
-      child: child,
-    );
-  }
 }
 
 class CustomPageTransitionBuilder extends PageTransitionsBuilder {
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:provider/provider.dart';
-
 import 'package:flutter/material.dart';
+
 import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
